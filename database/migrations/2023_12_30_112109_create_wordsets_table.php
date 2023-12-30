aacd0a09c89e6539a886654e3d2761c6ee57194a
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-12-30 16:30:12 +0500

    feat: Create wordsets table

---
+++ database/migrations/2023_12_30_112109_create_wordsets_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wordsets', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_puzzle_english');
+            $table->string('title');
+            $table->dateTimeTz('learnt_at')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wordsets');
+    }
+};
